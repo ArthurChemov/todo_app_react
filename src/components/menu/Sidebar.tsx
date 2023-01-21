6e255ff8f9572d7ae4e30a463a3b5280bf1f414c
Artur Chemov <arthur.chemov@gmail.com>
2023-01-21 04:57:00 +0200

    Update layout

--- src/components/menu/Sidebar.tsx
+++ src/components/menu/Sidebar.tsx
@@ -4,21 +4,21 @@ import { links } from './Links';
 
 function Sidebar() {
     return (
-        <Flex className='w-1/6 flex items-center flex-col font-serif pt-2'>
-            <Box className='pt-6 ml-3 w-3/4
-                small:text-xs medium:text-base large:text-xl
-                small:w-full  medium:w-full    large:w-3/4'
+        <Flex className=' h-full w-full fixed items-start flex-row'>
+            <Flex className='w-1/6 items-center flex-col font-serif pt-6 ml-3'
             >
                 {links.map((item, index) => {
                     return (
-                        <nav key={index} className='mb-2 hover:text-red-500 hover:border-4 hover:border-transparent hover:border-r-red-500'>
+                        <nav key={index} className='mb-2 hover:text-red-500 hover:border-4 hover:border-transparent hover:border-r-red-500
+                        small:text-xs medium:text-base large:text-xl
+                        small:w-full  medium:w-full    large:w-3/4'>
                         <NavLink to={item.path}>
                             <span>{item.name}</span>
                         </NavLink>
                         </nav>
                     );
                 })}
-            </Box>
+            </Flex>
         </Flex>
     );
 }
